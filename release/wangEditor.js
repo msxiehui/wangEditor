0dcc9f83ac7f246c41d9d27f7c48a95d6cef909d
msxiehui <msxiehui@163.com>
2020-09-11 15:14:32 +0800

    增加移动调试

--- release/wangEditor.js
+++ release/wangEditor.js
@@ -820,7 +820,6 @@ Bold.prototype = {
     // 点击事件
     onClick: function onClick(e) {
         // 点击菜单将触发这里
-        // alert('testdd')
         var editor = this.editor;
         var isSeleEmpty = editor.selection.isSelectionEmpty();
 
@@ -844,6 +843,64 @@ Bold.prototype = {
         var editor = this.editor;
         var $elem = this.$elem;
         if (editor.cmd.queryCommandState('bold')) {
+            this._active = true;
+            $elem.addClass('w-e-active');
+        } else {
+            this._active = false;
+            $elem.removeClass('w-e-active');
+        }
+    }
+};
+
+/*
+    menu - header
+*/
+// 构造函数
+function Head(editor) {
+    this.editor = editor;
+    this.$elem = $('<div class="w-e-menu"><i class="w-e-icon-header"></i></div>');
+    this.type = 'droplist';
+
+    // 当前是否 active 状态
+    this._active = false;
+    var _this = this;
+    this.$elem.on('click', function (e) {
+        console.log(_this.isH);
+        if (!_this.isH) {
+            _this._command('<h1>');
+            _this.isH = true;
+        } else {
+            _this._command('<p>');
+            _this.isH = false;
+        }
+    });
+}
+
+// 原型
+Head.prototype = {
+    constructor: Head,
+    isH: false,
+    // 执行命令
+    _command: function _command(value) {
+        var editor = this.editor;
+
+        var $selectionElem = editor.selection.getSelectionContainerElem();
+        if (editor.$textElem.equal($selectionElem)) {
+            // 不能选中多行来设置标题，否则会出现问题
+            // 例如选中的是 <p>xxx</p><p>yyy</p> 来设置标题，设置之后会成为 <h1>xxx<br>yyy</h1> 不符合预期
+            return;
+        }
+
+        editor.cmd.do('formatBlock', value);
+    },
+
+    // 试图改变 active 状态
+    tryChangeActive: function tryChangeActive(e) {
+        var editor = this.editor;
+        var $elem = this.$elem;
+        var reg = /^h/i;
+        var cmdValue = editor.cmd.queryCommandValue('formatBlock');
+        if (reg.test(cmdValue)) {
             this._active = true;
             $elem.addClass('w-e-active');
         } else {
@@ -999,84 +1056,6 @@ DropList.prototype = {
         // 隐藏并需改属性
         $container.hide();
         this._show = false;
-    }
-};
-
-/*
-    menu - header
-*/
-// 构造函数
-function Head(editor) {
-    this.editor = editor;
-    this.$elem = $('<div class="w-e-menu"><i class="w-e-icon-header"></i></div>');
-    this.type = 'droplist';
-
-    // 当前是否 active 状态
-    this._active = false;
-    var _this = this;
-    // 初始化 droplist
-    // this.droplist = new DropList(this, {
-    //     width: 100,
-    //     $title: $('<p>设置标题</p>'),
-    //     type: 'list', // droplist 以列表形式展示
-    //     list: [
-    //         { $elem: $('<h1>H1</h1>'), value: '<h1>' },
-    //         { $elem: $('<h2>H2</h2>'), value: '<h2>' },
-    //         { $elem: $('<h3>H3</h3>'), value: '<h3>' },
-    //         { $elem: $('<h4>H4</h4>'), value: '<h4>' },
-    //         { $elem: $('<h5>H5</h5>'), value: '<h5>' },
-    //         { $elem: $('<p>正文</p>'), value: '<p>' }
-    //     ],
-    //     onClick: (value) => {
-    //         // 注意 this 是指向当前的 Head 对象
-    //         this._command(value)
-    //     }
-    // })
-
-    this.$elem.on('click', function (e) {
-        // _this.onClick('<h1>');
-        console.log(_this.isH);
-        if (!_this.isH) {
-            _this._command('<h1>');
-            _this.isH = true;
-        } else {
-            _this._command('<p>');
-            _this.isH = false;
-        }
-    });
-}
-
-// 原型
-Head.prototype = {
-    constructor: Head,
-    isH: false,
-    // 执行命令
-    _command: function _command(value) {
-        var editor = this.editor;
-
-        var $selectionElem = editor.selection.getSelectionContainerElem();
-        if (editor.$textElem.equal($selectionElem)) {
-            // 不能选中多行来设置标题，否则会出现问题
-            // 例如选中的是 <p>xxx</p><p>yyy</p> 来设置标题，设置之后会成为 <h1>xxx<br>yyy</h1> 不符合预期
-            return;
-        }
-
-        editor.cmd.do('formatBlock', value);
-    },
-
-    // 试图改变 active 状态
-    tryChangeActive: function tryChangeActive(e) {
-        var editor = this.editor;
-        var $elem = this.$elem;
-        var reg = /^h/i;
-        var cmdValue = editor.cmd.queryCommandValue('formatBlock');
-        if (reg.test(cmdValue)) {
-            this._active = true;
-            $elem.addClass('w-e-active');
-        } else {
-            this._active = false;
-            $elem.removeClass('w-e-active');
-        }
     }
 };
 
--- src/js/menus/head/index.js
+++ src/js/menus/head/index.js
@@ -2,7 +2,6 @@
     menu - header
 */
 import $ from '../../util/dom-core.js'
-import DropList from '../droplist.js'
 
 // 构造函数
 function Head(editor) {
@@ -12,40 +11,17 @@ function Head(editor) {
 
     // 当前是否 active 状态
     this._active = false
-    let _this = this;
-    // 初始化 droplist
-    // this.droplist = new DropList(this, {
-    //     width: 100,
-    //     $title: $('<p>设置标题</p>'),
-    //     type: 'list', // droplist 以列表形式展示
-    //     list: [
-    //         { $elem: $('<h1>H1</h1>'), value: '<h1>' },
-    //         { $elem: $('<h2>H2</h2>'), value: '<h2>' },
-    //         { $elem: $('<h3>H3</h3>'), value: '<h3>' },
-    //         { $elem: $('<h4>H4</h4>'), value: '<h4>' },
-    //         { $elem: $('<h5>H5</h5>'), value: '<h5>' },
-    //         { $elem: $('<p>正文</p>'), value: '<p>' }
-    //     ],
-    //     onClick: (value) => {
-    //         // 注意 this 是指向当前的 Head 对象
-    //         this._command(value)
-    //     }
-    // })
-
+    let _this = this
     this.$elem.on('click', function (e) {
-        // _this.onClick('<h1>');
         console.log(_this.isH)
         if(!_this.isH){
-            _this._command('<h1>');
+            _this._command('<h1>')
             _this.isH = true
         }else{
-            _this._command('<p>');
+            _this._command('<p>')
             _this.isH = false
         }
-
-
-    });
-
+    })
 }
 
 // 原型
